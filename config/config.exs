56eabaa14d4a88273667235b5e901aafb7e26e42
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-09 12:03:12 -0500

    oauth request works

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,6 @@ config :ueberauth, Ueberauth,
   ]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: System.get_env("GITHUB_CLIENT_ID"),
-  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+  #client_id: System.get_env("GITHUB_CLIENT_ID"),
+  #client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
