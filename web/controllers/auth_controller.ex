d01055b9ecfd493b9285e403af9b8bfcf238f822
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-11 15:49:00 -0500

    adds user restrictions

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -21,7 +21,7 @@ defmodule Discuss.AuthController do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
         conn
-          |> put_flash(:info, "Welcome back #{user.login}!")
+          |> put_flash(:info, "Welcome back, #{user.login}!")
           |> put_session(:user_id, user.id) 
           |> redirect(to: topic_path(conn, :index))
       {:error, _reason} -> 
